dc5fb980fd875f0cef7a946d8c9b74af9e18516e
Caio Fernando <caiofsr13@gmail.com>
2021-12-16 18:06:24 -0300

    feat: :art: new refactors

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -11,15 +11,36 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("say hello to people", func(t *testing.T) {
-		got := Hello("Caio")
+		got := Hello("Caio", "")
 		want := "Hello, Caio"
 
 		verifyMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello World', if was passed empty string", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+
+		verifyMessage(t, got, want)
+	})
+
+	t.Run("in portuguese", func(t *testing.T) {
+		got := Hello("Caio", "portuguese")
+		want := "Olá, Caio"
+
+		verifyMessage(t, got, want)
+	})
+
+	t.Run("in french", func(t *testing.T) {
+		got := Hello("Caio", "french")
+		want := "Bonjour, Caio"
+
+		verifyMessage(t, got, want)
+	})
+
+	t.Run("in spanish", func(t *testing.T) {
+		got := Hello("Caio", "spanish")
+		want := "Hola, Caio"
 
 		verifyMessage(t, got, want)
 	})
