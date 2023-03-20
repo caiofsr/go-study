c5c9aaa7e90af0b285211c8b6f7d1638a67f75ff
Caio Fernando <caiofsr13@gmail.com>
2023-03-19 23:55:40 -0300

    commit

---
+++ 2/main.go
@@ -0,0 +1,17 @@
+package main
+
+const a = "Hello, World!"
+
+var (
+	b bool    = true
+	c int     = 10
+	d string  = "Caio"
+	e float64 = 1.2
+)
+
+func main() {
+	a := "X"
+	a = "asdf"
+
+	println(a)
+}
