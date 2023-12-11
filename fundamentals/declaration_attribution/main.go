94d6c55017656d2805f3e4333e37fa20d12d5150
Caio Fernando <caiofsr13@gmail.com>
2023-12-11 11:29:11 -0300

    declaração e atribuição

---
+++ fundamentals/declaration_attribution/main.go
@@ -0,0 +1,20 @@
+package main
+
+const a = "Hello World"
+
+var (
+	b bool    = true
+	c int     = 10
+	d string  = "Caio"
+	e float64 = 1.2
+)
+
+func main() {
+	a := "X"
+	a = "fd"
+	println(a)
+	println(b)
+	println(c)
+	println(d)
+	println(e)
+}
